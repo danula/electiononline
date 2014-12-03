9ee638139b2f15bbc46d72287a2c3ac7aa7d65a1
Danula Eranjith <hmdanulae@gmail.com>
2014-12-03 14:52:41 +0530

    district resutls

--- app/views/candidatesummary.blade.php
+++ app/views/candidatesummary.blade.php
@@ -5,13 +5,30 @@
 @endsection
 
 @section('content')
+<div class="row"><h3>{{$candidate[0]->name}}</h3></div>
+
 <div class="row">
-    {{$candidate[0]->name}}
 
-    @foreach($results as $r)
-        {{$r->seat->name}} - {{$r->number_of_votes}} - {{$seatresults[$r->seat->id-1]->Polled_votes}} <br>
-
+    <div class="col-lg-4">
+    <table class="table table-striped table-bordered table-hover">
+        <thead>
+        <tr>
+            <th>Seat</th>
+            <th>Percentage</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($percentages as $id=>$p)
+        @if(!str_contains($seatnames[$id],'Postal'))
+        <tr>
+            <td>{{$seatnames[$id]}}</td>
+            <td>{{number_format($p,2)}}</td>
+        </tr>
+    @endif
     @endforeach
+    </tbody>
+    </table>
+    </div>
 
 </div>
 @endsection
--- app/views/districtresult.blade.php
+++ app/views/districtresult.blade.php
@@ -1,7 +1,71 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: danula
- * Date: 12/3/14
- * Time: 12:40 PM
- */ 
+@extends('master')
+
+@section('content')
+<div class="row"><h3></h3></div>
+
+<div class="row">
+    <div id="piechart" class="panel-default panel-body" style="width: 100%; height: 62%;">
+    </div>
+</div>
+
+<div class="row">
+    <h4>{{$district->name}} District : Year {{$year}}</h4><br>
+    <div class="progress">
+      @foreach($candidates as $c)
+      @if($c->resultsd[0]->number_of_votes/$districtresult->valid_votes>0.1)
+
+      <div class="progress-bar progress-bar-{{$c->colour}}" style="width:{{$c->resultsd[0]->number_of_votes/$districtresult->valid_votes*100}}%" >
+            {{$c->name}} - {{number_format($c->resultsd[0]->number_of_votes/$districtresult->valid_votes*100,2)}}%
+      </div>
+      @endif
+      @endforeach
+
+    </div>
+</div>
+<div class="row">
+
+<table class="table table-striped table-bordered table-hover">
+    <thead>
+    <tr>
+        <th>Candidate</th>
+        <th>Votes</th>
+        <th>Percentage</th>
+    </tr>
+    </thead>
+    <tbody>
+    @foreach($candidates as $c)
+    <tr>
+        <td>{{{$c->name}}}</td>
+        <td>{{{$c->resultsd[0]->number_of_votes}}}</td>
+        <td>{{number_format($c->resultsd[0]->number_of_votes/$districtresult->valid_votes*100,2)}}%</td>
+    </tr>
+    @endforeach
+    <b>
+    <tr class="info">
+        <td>Polled Votes</td>
+        <td>{{{$districtresult->polled_votes}}}</td>
+        <td>{{number_format($districtresult->polled_votes/$districtresult->registered_votes*100,2)}}%</td>
+    </tr>
+    <tr class="info">
+        <td>Rejected Votes</td>
+        <td>{{{$districtresult->rejected_votes}}}</td>
+        <td></td>
+    </tr>
+    <tr class="info">
+        <td>Registered Votes</td>
+        <td>{{{$districtresult->registered_votes}}}</td>
+        <td></td>
+    </tr>
+    <tr class="info">
+        <td>Valid Votes</td>
+        <td>{{{$districtresult->valid_votes}}}</td>
+        <td></td>
+    </tr>
+
+    </b>
+    </tbody>
+
+</table>
+</div>
+
+@endsection
