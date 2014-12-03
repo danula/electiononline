4e5fbf263ea607cd2b1b40f1f52a4bbed77b30d9
SankaGIT <nuwan.ras@gmail.com>
2014-12-03 15:38:27 +0530

    added home page

---
+++ app/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('master')
+@section('content')
+<h1>Home</h1>
+<div class="row">
+    <div class="col-md-9">
+        <h2>Total Summary of past years</h2>
+    </div>
+    <div class="col-md-3">
+        <p>Online election results</p>
+    </div>
+</div>
+<div class="row">
+    <h2>grahic for this 2015 election</h2>
+</div>
+@stop
