f2b1d47be6c3f7b1ac0041e8f46fab5956cd0586
Danula Eranjith <hmdanulae@gmail.com>
2014-12-05 15:32:32 +0530

    figures correction

--- app/controllers/ResultController.php
+++ app/controllers/ResultController.php
@@ -140,8 +140,8 @@ class ResultController extends BaseController {
             'seatnames'=>$seatnames,
             'percentagesd'=>$percentagesd,
             'districtnames'=>$districtnames,
-            'years'=>array('1982'=>'1982','1994'=>'1994','1999'=>'1999','2005'=>'2005','2010'=>'2010'),
-            'totalvotes'=>array('1988'=>5094778,'1982'=>6522147,'1994'=>7561526,'1999'=>8435754,'2005'=>9717039,'2010'=>10393613),
+            'years'=>array('1982'=>'1982','1988'=>'1988','1994'=>'1994','1999'=>'1999','2005'=>'2005','2010'=>'2010'),
+            'totalvotes'=>array('1982'=>6522147,'1988'=>5094778,'1994'=>7561526,'1999'=>8435754,'2005'=>9717039,'2010'=>10393613),
             'colour'=>array('active'=>'primary','success'=>'green','warning'=>'yellow','danger'=>'red','info'=>'info')
         );
         return View::make('candidatesummary',$data);
