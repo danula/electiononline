dd48f80c8c1fb568792e79c1f4b0c17b0b84355c
Madawa Soysa <madawa.rc@gmail.com>
2014-12-10 17:42:16 +0530

    home charts

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -1,5 +1,3 @@
-
-{{HTML::script("https://www.google.com/jsapi");}}
 @extends('master')
 @section('content')
 <?php
@@ -32,22 +30,24 @@
     </div>
 <div class="row">
     <div class="col-md-9">
-        <div class="panel panel-default">
-            <div class="panel-heading">
-                <a href="{{URL::to('districtplot/Colombo')}}">Total Summary of Past Years</a>
+        <div class="box box-primary">
+            <div class="box-header">
+                <a class="box-title" href="{{URL::to('districtplot/Colombo')}}">Total Summary of Past Years</a>
             </div>
-            <div class="panel-body">
-                <div id="overall_line_chart" style="min-height: 294px"></div>
+            <div class="box-body chart-responsive">
+                <div class="chart" id="overall_line_chart" style="height: 325px"></div>
             </div>
         </div>
     </div>
     <div class="col-md-3">
-        <div class="panel panel-primary">
-            <div class="panel-heading">Online election result</div>
-            <div class="panel-body">
-                <div clas="row"><div id="piechart"></div></div>
-                <p>Total Votes: <?php echo $votes?></p>
-                <a href="http://forum.chandaya.info/discussion/2/online-election-2015" class="btn btn-primary btn-block">Vote Now</a>
+        <div class="box box-primary">
+            <div class="box-header">
+                <h3 class="box-title">Online Election Result</h3>
+            </div>
+            <div class="box-body chart-responsive">
+                <div class="chart" style="height: 258px" id="online_election_piechart"></div>
+                <h4>Total Votes: <?php echo $votes?></h4>
+                <a href="http://forum.chandaya.info/discussion/2/online-election-2015" class="btn btn-danger btn-block">Vote Now</a>
             </div>
         </div>
     </div>
@@ -57,59 +57,43 @@
         <a href="http://forum.chandaya.info"> <img class="img-responsive" src="../resources/banner.png" ></a>
     </div>
 </div>
+<!-- Morris.js charts -->
+{{HTML::script("//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js");}}
+{{HTML::script("../../js/plugins/morris/morris.min.js");}}
 <script type="text/javascript">
+
     var MR = document.getElementById("mahinda");
     var MY3= document.getElementById("my3");
-    google.load("visualization", "1", {packages:["corechart"]});
-    google.setOnLoadCallback(drawChart);
+    $(function() {
+        "use strict";
+        new Morris.Line({
+            element: 'overall_line_chart',
+            resize: true,
+            data: [
+                {"year":"1982","win":3450813,"winP":"UNP","sec":2546438,"secP":"SLFP","oth":522675},
+                {"year":"1988","win":2569199,"winP":"UNP","sec":2289857,"secP":"SLFP","oth":235701},
+                {"year":"1994","win":2715283,"winP":"UNP","sec":4709205,"secP":"PA","oth":137039},
+                {"year":"1999","win":3602748,"winP":"UNP","sec":4312157,"secP":"PA","oth":520849},
+                {"year":"2005","win":4706366,"winP":"UNP","sec":4887152,"secP":"UPFA","oth":124241},
+                {"year":"2010","win":4173185,"winP":"NDF","sec":6015934,"secP":"UPFA","oth":204494}
+            ],
+            xkey: 'year',
+            ykeys: ['win','sec','oth'],
+            lineColors: ['#53A336','#0070D1', '#FF8000'],
+            hideHover: 'false'
+        });
 
-    function drawChart() {
-
-        var data = google.visualization.arrayToDataTable([
-                         ['Party', 'Votes'],
-                         ['Mahinda Rajapaksha',     parseFloat(MR.value)],
-                         ['Mithreepala Sirisena',     parseFloat(MY3.value)]
-        ]);
-
-        var options = {
-            is3D: true,
-            colors: ['blue', 'green'],
-            backgroundColor: 'transparent',
-            legend: {position: 'none'},
-            height: 230
-
-        };
-
-        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-        chart.draw(data, options);
-    }
+        new Morris.Donut({
+            element: 'online_election_piechart',
+            resize: true,
+            colors: ["#0070D1", "#53A336"],
+            data: [
+                {label: "Mahinda Rajapaksha", value: parseFloat(MR.value)},
+                {label: "Maithripala Sirisena", value: parseFloat(MY3.value)}
+            ],
+            hideHover: 'false'
+        });
+    });
 </script>
-@stop
 
-<script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawDistrictChart);
-      function drawDistrictChart() {
-        var data = new google.visualization.DataTable();
-                data.addColumn('string', 'Year');
-                data.addColumn('number', 'Votes');
-                data.addColumn({type: 'string', role: 'annotation'});
-                data.addColumn('number', 'Votes');
-                data.addColumn({type: 'string', role: 'annotation'});
-                data.addColumn('number', 'Votes');
-                data.addColumn({type: 'string', role: 'annotation'});
-                data.addRows(<?php echo(json_encode($chartData)); ?>);
-
-        var options = {
-          pointSize: 5,
-          colors:['green', 'blue', 'orange'],
-          annotation: {3: {style: 'line'}, 5: {style: 'line'}, 7: {style: 'line'}},
-          legend:'none'
-        };
-
-        var chart = new google.visualization.LineChart(document.getElementById('overall_line_chart'));
-
-        chart.draw(data, options);
-      }
-</script>
+@endsection
