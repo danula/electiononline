51e22a6dcacde81a4dbd4c9502047b77e81c288a
Madawa Soysa <madawa.rc@gmail.com>
2014-12-12 10:12:13 +0530

    layout

--- app/views/districtplot.blade.php
+++ app/views/districtplot.blade.php
@@ -1,60 +1,28 @@
 @extends('master')
 @section('content')
-
-
 <nav style="margin-top: 20px; padding-left: 30px; background-color: transparent" class="navbar navbar-default" >
-      <div class="row">
-            {{ Form::open(array('url'=>'districtplot','name'=>'changeresult','class'=>'navbar-form navbar-left')) }}
-
-            District&nbsp;
-
-            {{Form::select('district_id',$districts,$district[0]->id,array('class'=>'form-control','onchange'=>"this.form.submit()"))}}
+    <div class="row">
+        {{ Form::open(array('url'=>'districtplot','name'=>'changeresult','class'=>'navbar-form navbar-left')) }}
+        District&nbsp;
+        {{Form::select('district_id',$districts,$district[0]->id,array('class'=>'form-control','onchange'=>"this.form.submit()"))}}
     </div>
 </nav>
 <div class="page-header">
     <h2> {{$district[0]->name}} District</h2>
 </div>
+<!-- Top Row -->
 <div class="row">
     <div class="col-md-3 col-sm-12 col-xs-12">
         <div style="margin-top: 6.5%; padding-right: 0;">
             <img src="../district2.png" width=230 usemap="#map" id="myImage" name="myImage"></img>
             <map id="map" name="map"></map>
         </div>
-
     </div>
     <div class="col-md-9 col-sm-12 col-xs-12">
         <div class="box box-primary">
             <div class="box-body chart-responsive">
                 <div class="chart" id="district_summary_line" style="height: 325px"></div>
             </div>
-        </div>
-
-    </div>
-</div>
-<div class="row">
-    <div class="col-md-12 col-sm-12 col-xs-12">
-
-        <div class="panel-group" id="accordion2" role="tablist" aria-multiselectable="true">
-        <h4>Seat Statistics</h4>
-        @foreach($seats as $seat)
-
-          <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="headingOne_{{$seat->id}}">
-              <h4 class="panel-title">
-                <a style="display: block; width: 100%" data-toggle="collapse" data-parent="#accordion2" href="#collapseOne_{{$seat->id}}" aria-expanded="false" aria-controls="collapseOne_{{$seat->id}}">
-                  {{$seat->name}} <i class="fa fa-bar-chart-o fa-fw pull-right"></i>
-                </a>
-              </h4>
-            </div>
-            <div id="collapseOne_{{$seat->id}}" class="panel-collapse collapse" role="tabpanel" aria-labelledby="headingOne_{{$seat->id}}">
-              <div align="center" class="box-body chart-responsive">
-                <div class="chart" id="linechart_{{$seat->id}}"></div>
-              </div>
-            </div>
-          </div>
-
-          @endforeach
-
         </div>
     </div>
 </div>
@@ -77,7 +45,7 @@
                         s = s +",";
                     num = parseFloat(res[i])*width/612.0;
                     s=s+num.toString();
-                    
+
                   }
                   $(this).attr("coords",  s);
                   //give the href to a district.
@@ -107,19 +75,16 @@
                     ]
               });
               $('#myImage').mapster('tooltip','alt');
-              
+
           })
-          
+
         });
 </script>
 
 <!-- Morris.js charts -->
 {{HTML::script("//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js");}}
-{{HTML::script("../../js/plugins/morris/morris.min.js");}}
-
-
+{{HTML::script("/js/plugins/morris/morris.min.js");}}
 <script type="text/javascript">
-
     Morris.Line({
         element: 'district_summary_line',
         resize: true,
@@ -143,32 +108,37 @@
     });
 </script>
 
-{{HTML::script("https://www.google.com/jsapi");}}
-<script type="text/javascript">
-      google.load("visualization", "1", {packages:["corechart"]});
-      google.setOnLoadCallback(drawSeatCharts);
-      function drawSeatCharts() {
-        var options = {
-            pointSize: 5,
-            colors:['green', 'blue', 'orange'],
-            annotation: {3: {style: 'line'}, 5: {style: 'line'}, 7: {style: 'line'}},
-            legend:'none',
-            width: '900'
-        };
-        var seatChartData = JSON.parse('{{json_encode($seatChartData)}}');
-        for (var key in seatChartData) {
-              var data = new google.visualization.DataTable();
-              data.addColumn('string', 'Year');
-              data.addColumn('number', 'Votes');
-              data.addColumn({type: 'string', role: 'annotation'});
-              data.addColumn('number', 'Votes');
-              data.addColumn({type: 'string', role: 'annotation'});
-              data.addColumn('number', 'Votes');
-              data.addColumn({type: 'string', role: 'annotation'});
-              data.addRows(seatChartData[key]);
-              new google.visualization.LineChart(document.getElementById('linechart_'+key)).draw(data, options);
-        }
-      }
-</script>
 
+<!-- Jquery UI -->
+{{HTML::script("//code.jquery.com/ui/1.11.1/jquery-ui.min.js");}}
+<!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+{{HTML::script("/js/AdminLTE/dashboard.js");}}
+
+<!-- Seat Statistics -->
+<div class="row">
+    <!-- Left col -->
+    <section class="col-lg-6 col-md-6 col-sm-12 connectedSortable">
+        <!-- Seat box -->
+        <div class="box box-default">
+            <div class="box-header">
+                <h3 class="box-title">Colombo North</h3>
+            </div>
+            <div class="box-body">
+
+            </div><!-- /.seat body -->
+        </div><!-- /.box (seat box) -->
+    </section><!-- /.Left col -->
+    <!-- right col -->
+    <section class="col-lg-6 col-md-6 col-sm-12 connectedSortable">
+        <!-- Seat box -->
+        <div class="box box-default">
+            <div class="box-header">
+                <h3 class="box-title">Colombo South</h3>
+            </div>
+            <div class="box-body">
+
+            </div><!-- /.seat body -->
+        </div><!-- /.box (seat box) -->
+    </section><!-- right col -->
+</div>
 @endsection
